84e6c25139131a55a56a63ab5136a1121da8d765
agasha54 <ivoig@bk.ru>
2021-06-24 17:17:35 +0300

    MVCMOVI-

--- MVCMOVIЕ/Startup.cs
+++ MVCMOVIЕ/Startup.cs
@@ -1,3 +1,5 @@
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace MVCMOVIЕ
 {
@@ -24,6 +27,9 @@ namespace MVCMOVIЕ
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
